993737df753fafc828588efb53837a00a9e006b2
misdake <misdake@gmail.com>
2026-04-18 00:06:22 +0800

    refactor: keep only compass render loop and pointer base

--- practice-tool/src/lib.rs
+++ practice-tool/src/lib.rs
@@ -14,10 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-mod config;
 mod practice_tool;
 mod util;
-mod widgets;
 mod map;
 
 use std::ffi::c_void;
